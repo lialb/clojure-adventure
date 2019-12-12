263fc66f79437fdd35f7a6c352ce3af7be814492
Matthew Pham <matthewpham55@gmail.com>
2019-12-11 22:03:22 -0600

    some basic functions

--- interaction/adventure/src/adventure/core.clj
+++ interaction/adventure/src/adventure/core.clj
@@ -1,7 +1,50 @@
 (ns adventure.core
   (:gen-class))
 
+(def player
+  { :location :foyer
+    :inventory #{}
+    :hp 10
+    :lives 1
+    :seen #{}})
+
+(defn reduceHealth [dmg]
+  (def player (update player :hp - dmg))
+  (if (<= (player :hp) 0)
+    (do (println "Game Over! You Died :(") (System/exit 0))
+  )
+)
+
+(defn removeFromInventory [item]
+  (def player 
+    (assoc player :inventory (disj (player :inventory) item))
+    )
+)
+
+(defn restoreHealth [health]
+  (def player (update player :hp + health))
+)
+
+(defn addToInventory [item]
+  (def player 
+    (assoc player :inventory (conj (player :inventory) item))
+    )
+)
+
+(defn printPlayer []
+  (println (str "Player is currently has " (player :hp) " hp and has " (player :inventory) " in their inventory."))
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;(println "Hello, World!")
+  (printPlayer)
+  (reduceHealth 5)
+  (addToInventory "key")
+  (printPlayer)
+  (removeFromInventory "key")
+  (printPlayer)
+  )
+
+(-main)
